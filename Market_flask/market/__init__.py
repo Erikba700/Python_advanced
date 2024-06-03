699597904ca31587316991b911d4543613a87760
Erik Badalyan <erik.badalyan700@gmail.com>
2024-06-03 21:32:03 +0400

    dotenv + readme.md

--- Market_flask/market/__init__.py
+++ Market_flask/market/__init__.py
@@ -1,11 +1,17 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
+load_dotenv()  # loading personal and sensitive data to the current process
+
 app = Flask(__name__)
+db_username = os.getenv("DB_USERNAME")
+db_password = os.getenv("DB_PASSWORD")
 app.config['SECRET_KEY'] = '0336fae0040444e76651d0c9'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Addcry700@localhost/market'
+app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{db_username}:{db_password}@localhost/market"
 bycrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = "login_page"
--- README.md
+++ README.md
@@ -1,5 +1,10 @@
+# Demo market
 This is a demo market site project where you can register in the site,
 log in and log out your account and see the changes in the site.
 
 This project teaches me how to work with forms, their validations,
 databases and mostly with User Authentication.
+
+### Contacts
+If you have any questions you can contact me via
+erik.badalyan700@gmail.com
